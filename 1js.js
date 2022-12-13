8068b4b3454fe958b3cd1d6b7f3a03624e84898c
Barannik Polina <barannik.polina@mail.ru>
2022-12-13 10:50:20 +0300

    Созданы HTML файлы

---
+++ 1js.js
@@ -0,0 +1,5 @@
+const celsius = Number.parseInt(prompt ("Введите температуру в градусах Цельсия"));
+alert(`Цельсий: ${celsius}, Фаренгейт: ${Math.round (fahrenheit = 1.8 * celsius + 32)}`);
+function celsiusToFahrenheit(celsius) {
+     return fahrenheit;
+}
---
+++ 2js.js
@@ -0,0 +1,5 @@
+const yourName = prompt (`Введите Ваше имя`);
+function greeting(yourName) {
+    console.log(`Привет, ${yourName}!`);
+}
+greeting(yourName);
